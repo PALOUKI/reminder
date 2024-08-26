b80c6a9b894280982d822c2f1cc9a6930987b98e
PALOUKI Godbless <paloukigodbless39@gmail.com>
2024-08-26 02:23:02 +0000

    calculator

--- sql/dbase.sql
+++ sql/dbase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 25 août 2024 à 16:16
+-- Généré le : lun. 26 août 2024 à 04:18
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -29,15 +29,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `images` (
   `image_id` int(11) NOT NULL,
-  `image` text NOT NULL
+  `image` text NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `images`
 --
 
-INSERT INTO `images` (`image_id`, `image`) VALUES
-(42, 'photo_2024-08-07_00-45-17.jpg');
+INSERT INTO `images` (`image_id`, `image`, `user_id`) VALUES
+(75, 'photo_2024-08-07_00-45-17.jpg', 40);
 
 -- --------------------------------------------------------
 
@@ -51,15 +52,16 @@ CREATE TABLE `reminders` (
   `event` text NOT NULL,
   `date` date NOT NULL,
   `importance` varchar(11) NOT NULL,
-  `statut` varchar(11) NOT NULL
+  `statut` varchar(11) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `reminders`
 --
 
-INSERT INTO `reminders` (`reminder_id`, `title`, `event`, `date`, `importance`, `statut`) VALUES
-(36, 'EventName', 'Event description. bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla', '2024-08-25', 'low', 'ended');
+INSERT INTO `reminders` (`reminder_id`, `title`, `event`, `date`, `importance`, `statut`, `user_id`) VALUES
+(68, 'aaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaa', '2024-08-26', 'low', 'ended', 40);
 
 -- --------------------------------------------------------
 
@@ -81,7 +83,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `name`, `firstName`, `email`, `password`) VALUES
 (39, 'Alaise', 'tchindou', 'Alaise.tchindou@exemple.com', '$2y$10$Ip/HOhf2.7FWZlZZjeeCF.aILwvA4b4fs2QYVYFWMC1Ojt8U56ylG'),
-(40, 'PALOUKI', 'Godbless', 'paloukigodbless39@gmail.com', '$2y$10$su08BYkGKel1IPKEnGhZ7uWkqv7N.NaP2jURYgfxtPp8C4/4TBSMu');
+(40, 'PALOUKI', 'Godbless', 'paloukigodbless39@gmail.com', '$2y$10$su08BYkGKel1IPKEnGhZ7uWkqv7N.NaP2jURYgfxtPp8C4/4TBSMu'),
+(41, 'victor', 'prenom', 'victor.prenom@exemple.com', '$2y$10$NMkz/al39xszKlfvFIJaC.Xo5RaYbwOaeinjLj28HCz7bV6KwLai6');
 
 --
 -- Index pour les tables déchargées
@@ -91,13 +94,15 @@ INSERT INTO `users` (`user_id`, `name`, `firstName`, `email`, `password`) VALUES
 -- Index pour la table `images`
 --
 ALTER TABLE `images`
-  ADD PRIMARY KEY (`image_id`);
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Index pour la table `reminders`
 --
 ALTER TABLE `reminders`
-  ADD PRIMARY KEY (`reminder_id`);
+  ADD PRIMARY KEY (`reminder_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Index pour la table `users`
@@ -113,19 +118,35 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `images`
 --
 ALTER TABLE `images`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT pour la table `reminders`
 --
 ALTER TABLE `reminders`
-  MODIFY `reminder_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `reminder_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `reminders`
+--
+ALTER TABLE `reminders`
+  ADD CONSTRAINT `reminders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
