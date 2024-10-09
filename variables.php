55973b4708ea0b3abb4e81687452c8b5b8cb6b59
PALOUKI SOBO Eham Godbless <161507086+PALOUKI@users.noreply.github.com>
2024-10-09 12:05:00 +0000

    Update variables.php

--- variables.php
+++ variables.php
@@ -1,40 +1,4 @@
 <?php
-    // $users=[
-    //    [
-    //      'full_name'=>'Alaise tchindou', 
-    //      'email'=>'Alaise.tchindou@exemple.com',
-    //      'password'=>'oklm',
-    //    ],
-    //    [
-    //       'full_name'=>'Nom victor',
-    //      'email'=>'Nom.victor@exemple.com',
-    //      'password'=>'oklm',
-    //    ]
-    //    ];
-        // $reminders=[
-        //     [
-        //         'title'=>'Evala',
-        //         'event'=>'Evala est très très reconnu et pratiqué en pays kabyè. On parle là de la fête ou les hommes soulève les autres hommes.',
-        //         'date'=>'',
-        //         'importance'=>'high',
-        //         'statut'=>'Coming'
-
-        //     ],
-        //     [
-        //         'title'=>'Akpéma',
-        //         'event'=>'Akpéma est très très reconnu et pratiqué en pays kabyè. On parle là de la fête ou les femmes marchent sur la montagne haha.',
-        //         'date'=>'',
-        //         'importance'=>'low',
-        //         'statut'=>'ended'
-
-        //     ]
-        //     ];
-
-
-
-
-
-
 
     require_once('config/dbconnect.php');
 
